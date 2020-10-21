5d63ebbb857e6153cedb3b5795ccf25ff94e2578
PhilipJohnson0229 <philip.johnson0229@gmail.com>
2020-10-21 00:35:00 -0400

    added a comment to PlayerAttacker script to test git

--- PlayerAttacker.cs
+++ PlayerAttacker.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//adding this comment to see a change happen in git
 namespace LOD 
 {
     public class PlayerAttacker : MonoBehaviour
