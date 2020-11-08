0986efb8bf296e64acfc795ef5ac67e6291851b4
PhilipJohnson0229 <philip.johnson0229@gmail.com>
2020-11-08 02:10:19 -0500

    weapon slots functionality

--- PlayerInventory.cs
+++ PlayerInventory.cs
@@ -11,6 +11,14 @@ namespace LOD
         public WeaponItem rightWeapon;
         public WeaponItem leftWeapon;
 
+        public WeaponItem unarmedWeapon;
+
+        public WeaponItem[] weaponInRightHandSlots = new WeaponItem[1];
+        public WeaponItem[] weaponInLeftHandSlots = new WeaponItem[2];
+
+        public int currentRightWeaponIndex = 0;
+        public int currentLeftWeaponIndex = 0;
+
         public void Awake()
         {
             weaponSlotManager = GetComponentInChildren<WeaponSlotManager>();
@@ -18,9 +26,44 @@ namespace LOD
 
         private void Start()
         {
+            rightWeapon = weaponInRightHandSlots[currentRightWeaponIndex];
+            leftWeapon = weaponInLeftHandSlots[currentLeftWeaponIndex];
             weaponSlotManager.LoadWeaponOnSlot(rightWeapon, false);
             weaponSlotManager.LoadWeaponOnSlot(leftWeapon, true);
         }
+
+        public void ChangeRightWeapon() 
+        {
+            currentRightWeaponIndex = currentRightWeaponIndex + 1;
+
+            if (currentRightWeaponIndex == 0 && weaponInRightHandSlots[0] != null)
+            {
+                rightWeapon = weaponInRightHandSlots[currentRightWeaponIndex];
+                weaponSlotManager.LoadWeaponOnSlot(weaponInRightHandSlots[currentRightWeaponIndex], false);
+            }
+            else if (currentRightWeaponIndex == 0 && weaponInRightHandSlots[0] == null) 
+            {
+                currentRightWeaponIndex = currentRightWeaponIndex + 1;
+            }
+            else if (currentRightWeaponIndex == 1 && weaponInRightHandSlots[1] != null)
+            {
+                rightWeapon = weaponInRightHandSlots[currentRightWeaponIndex];
+                weaponSlotManager.LoadWeaponOnSlot(weaponInRightHandSlots[currentRightWeaponIndex], false);
+            }
+            else 
+            {
+                currentRightWeaponIndex = currentRightWeaponIndex + 1;
+            }
+
+            if (currentRightWeaponIndex > weaponInRightHandSlots.Length - 1) 
+            {
+                currentRightWeaponIndex = -1;
+                rightWeapon = unarmedWeapon;
+                weaponSlotManager.LoadWeaponOnSlot(unarmedWeapon, false);
+            }
+        }
+
+
     }
 
 }
